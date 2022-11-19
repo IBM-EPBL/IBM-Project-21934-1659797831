085daea695f650ddf802e7416a7078fb0c091524
Sekarmoorthy <66151362+sekarmoorthy@users.noreply.github.com>
2022-11-19 19:28:18 +0530

    Update app.py

--- Assessments/Team Member 2 - Sekarmoorthy/Assignment 3/app.py
+++ Assessments/Team Member 2 - Sekarmoorthy/Assignment 3/app.py
@@ -3,8 +3,8 @@ import ibm_boto3
 from ibm_botocore.client import Config, ClientError
 
 COS_ENDPOINT = "https://control.cloud-object-storage.cloud.ibm.com/v2/endpoints" # Current list avaiable at https://control.cloud-object-storage.cloud.ibm.com/v2/endpoints
-COS_API_KEY_ID = "U3llyR48g6rTfEXE15Km2XRhKkrtTNjWE1wH-0jeLd0C" # eg "W00YixxxxxxxxxxMB-odB-2ySfTrFBIQQWanc--P3byk"
-COS_INSTANCE_CRN = "crn:v1:bluemix:public:iam-identity::a/ed1ba8ed327c43b4a5cff9c96489fd22::serviceid:ServiceId-7ec24ff0-6745-481b-b2cb-1e8435f5b378" # eg "crn:v1:bluemix:public:cloud-object-storage:global:a/3bf0d9003xxxxxxxxxx1c3e97696b71c:d6f04d83-6c4f-4a62-a165-696756d63903::"
+COS_API_KEY_ID = "*************************************" # eg "W00YixxxxxxxxxxMB-odB-2ySfTrFBIQQWanc--P3byk"
+COS_INSTANCE_CRN = "crn:v1:bluemix:public:iam-identity::a/****************************::serviceid:ServiceId-7ec24ff0-6745-481b-b2cb-****************" # eg "crn:v1:bluemix:public:cloud-object-storage:global:a/3bf0d9003xxxxxxxxxx1c3e97696b71c:d6f04d83-6c4f-4a62-a165-696756d63903::"
 
 cos = ibm_boto3.resource("s3",
     ibm_api_key_id=COS_API_KEY_ID,
